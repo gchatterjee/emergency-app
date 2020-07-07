41317e77c1bcf3f9f986d25337e0bb604b253d30
Gaurav Chatterjee <chatterjeegaur@gmail.com>
2020-07-06 22:29:10 -0400

    added results page and routing to results page

--- app/components/CardWidget/CardWidget.tsx
+++ app/components/CardWidget/CardWidget.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Card } from 'react-native-paper';
 import { SearchResult } from '../SearchWidget/SearchResults';
 import CallButton from './CallButton';
@@ -8,8 +9,14 @@ class CardWidget extends React.Component<{ resource: SearchResult }> {
   render(): JSX.Element {
     const { resource } = this.props;
 
+    const styles = StyleSheet.create({
+      card: {
+        marginBottom: 10,
+      },
+    });
+
     return (
-      <Card>
+      <Card style={styles.card}>
         <Card.Title title={resource.name}></Card.Title>
         <Card.Actions>
           <CallButton phoneNumber={resource.phoneNumber}></CallButton>
